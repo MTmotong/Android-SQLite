3f961a4943511631f4fdaed354c794da5412edf8
dzy <dzymt6627@163.com>
2016-12-01 16:59:16 +0800

    create database

--- src/com/example/databasetest/MainActivity.java
+++ src/com/example/databasetest/MainActivity.java
@@ -4,33 +4,29 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 
 
 public class MainActivity extends Activity {
-
+	
+	private MyDatabaseHelper dbHelper;
+	
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //通过构造函数的参数，将数据库名指定为Bookstore。db，版本号指定为1
+        dbHelper = new MyDatabaseHelper(this, "Bookstore.db", null, 1);
+        Button createDatabase = (Button) findViewById(R.id.create_database);
+        createDatabase.setOnClickListener(new OnClickListener() {
+        	@Override
+        	public void onClick(View v) {
+        		dbHelper.getWritableDatabase(); 
+        		//第一次点击按钮时，会检测到当前程序中并没有此数据库，于是会创建数据库并调用MyDatabaseHelper中的onCreate方法
+        	}
+        });
     }
 
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
-    }
 }
---
+++ src/com/example/databasetest/MyDatabaseHelper.java
@@ -0,0 +1,37 @@
+package com.example.databasetest;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteDatabase.CursorFactory;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.widget.Toast;
+
+public class MyDatabaseHelper extends SQLiteOpenHelper {
+	
+	public static final String GREATE_BOOK = "create table Book ("
+			+ "id integer primary key autoincrement, "
+			+ "author text, "
+			+ "price real, "
+			+ "pages integer, "
+			+ "name text)";
+	
+	private Context mContext;
+	
+	//自己建的帮助类，继承自SQLiteOpenHelper，在这里重写onCreate和onUpgrate，在这两个方法中实现创建、升级数据库的逻辑
+	public MyDatabaseHelper(Context context, String name, CursorFactory factory, int version) {
+		super(context, name, factory, version);
+		mContext = context;
+	}
+	
+	@Override
+	public void onCreate(SQLiteDatabase db) {
+		
+		db.execSQL(GREATE_BOOK); //调用execSQL方法去执行建表语句
+		Toast.makeText(mContext, "Create successed", Toast.LENGTH_SHORT).show();
+	}
+	@Override
+	public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+		
+	}
+	
+}
