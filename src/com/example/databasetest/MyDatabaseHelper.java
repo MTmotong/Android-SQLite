9a65bd64ccb59983397391f42cb3921289371b09
dzy <dzymt6627@163.com>
2016-12-02 00:48:37 +0800

    update in reality

--- src/com/example/databasetest/MyDatabaseHelper.java
+++ src/com/example/databasetest/MyDatabaseHelper.java
@@ -13,7 +13,8 @@ public class MyDatabaseHelper extends SQLiteOpenHelper {
 			+ "author text, "
 			+ "price real, "
 			+ "pages integer, "
-			+ "name text)";
+			+ "name text, "
+			+ "category_id integer)";
 	
 	public static final String GREATE_CATEGORY = "create table Category (" 
 			+ "id integer primary key autoincrement, "
@@ -37,9 +38,14 @@ public class MyDatabaseHelper extends SQLiteOpenHelper {
 	}
 	@Override
 	public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
-		db.execSQL("drop table if exists Book");
-		db.execSQL("drop table if exists Category");
-		onCreate(db);
+		switch (oldVersion) {
+			case 1:
+				db.execSQL(GREATE_CATEGORY);
+			case 2:
+				db.execSQL(GREATE_CATEGORY);
+			default:
+		
+		}
 	}
 	
 }
